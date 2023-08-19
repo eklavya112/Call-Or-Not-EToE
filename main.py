8346fe5768792dbc756dd28c36034f677636df83
Mahesh Singh Negi <108079474+eklavya112@users.noreply.github.com>
2023-08-19 19:20:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ df = get_data("Bank.csv",";")
 final_df = ftr_eng_sel(df)
 
 #Training the model
-trained_model(final_df,[0,1,2,4,5])
+trained_model(final_df,[0,1,2,3,5,6])
 
 model = joblib.load("trained_model.pkl")
 J = ['housemaid', 'services', 'admin.', 'blue-collar', 'technician',
